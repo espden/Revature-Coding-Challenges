ec8cae95073a530381db42c4b45d8c4f18e7b61d
Denis Espino <despino6160.com>
2023-03-23 17:15:20 -0400

    Created coding challenge for 03-23

---
+++ 03-23/Program.cs
@@ -0,0 +1,8 @@
+﻿int CircuitPower(int voltage, int current) 
+{
+    return voltage * current;
+}
+
+System.Console.WriteLine(CircuitPower(230, 10) == 2300);
+System.Console.WriteLine(CircuitPower(110, 3) == 330);
+System.Console.WriteLine(CircuitPower(480, 20) == 9600);
