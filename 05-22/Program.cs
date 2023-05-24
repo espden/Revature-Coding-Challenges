bc3339124b4b263db781b19efeab471afb2fcaa7
Denis Espino <despino6160.com>
2023-05-24 11:07:21 -0400

    05-22

---
+++ 05-22/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        SecondMaximumNumberChallenge();
+        SumDigitsInStringChallenge();
+    }
+
+    public static void SecondMaximumNumberChallenge()
+    {
+        int tripleCount = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < tripleCount; i++)
+        {
+            string tripleString = Console.ReadLine();
+            int[] triple = tripleString.Split(" ").Select(str => Int32.Parse(str)).ToArray();
+            int mid = Mid(triple[0], triple[1], triple[2]);
+            Console.WriteLine(mid);
+        }
+    }
+
+    // Returns the middle number of three numbers
+    public static int Mid(int a, int b, int c) 
+    {
+        if (a > b && a > c)
+        {
+            return (b > c) ? b : c;
+        } 
+        else if (b > a && b > c)
+        {
+            return (a > c) ? a : c;
+        }
+        else
+        {
+            return (a > b) ? a : b;
+        }
+    }
+
+    public static void SumDigitsInStringChallenge()
+    {
+        int testCases = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < testCases; i++)
+        {
+            string str = Console.ReadLine();
+            int sum = SumDigitsInString(str);
+            Console.WriteLine(sum);
+        }
+    }
+
+    // Returns the sum of all digits in the string
+    public static int SumDigitsInString(string str)
+    {
+        int sum = 0;
+        foreach(char c in str)
+        {
+            if (Char.IsDigit(c))
+            {
+                int digit = Int32.Parse(c.ToString());
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+}
